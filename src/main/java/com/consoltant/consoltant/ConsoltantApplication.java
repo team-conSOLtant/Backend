8408106e19e2dcea66dece5b6c40b96bcd1e6960
DDongYul <comeme0101@naver.com>
2024-08-07 21:55:27 +0900

    init: project initial commit

---
+++ src/main/java/com/consoltant/consoltant/ConsoltantApplication.java
@@ -0,0 +1,13 @@
+package com.consoltant.consoltant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsoltantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsoltantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/consoltant/consoltant/ConsoltantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.consoltant.consoltant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsoltantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
