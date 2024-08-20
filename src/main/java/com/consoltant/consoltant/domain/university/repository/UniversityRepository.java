8b7a65c0f00b76bdf6155d8d1e84179875305560
박상욱 <dkxkqkrtkddn@naver.com>
2024-08-20 21:16:59 +0900

    feat: User Repository 추가

--- src/main/java/com/consoltant/consoltant/domain/university/repository/UniversityRepository.java
+++ src/main/java/com/consoltant/consoltant/domain/university/repository/UniversityRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UniversitryRepository extends JpaRepository<University,Long> {
+public interface UniversityRepository extends JpaRepository<University,Long> {
 }
