062d059d7601a2c262d247cc4d546d388ff722e8
DDongYul <comeme0101@naver.com>
2024-08-21 23:02:28 +0900

    feat: 고객 여정 API 개발

---
+++ src/main/java/com/consoltant/consoltant/domain/journey/repository/JourneyRepository.java
@@ -0,0 +1,11 @@
+package com.consoltant.consoltant.domain.journey.repository;
+
+import com.consoltant.consoltant.domain.journey.entity.Journey;
+import com.consoltant.consoltant.util.constant.JourneyType;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface JourneyRepository extends JpaRepository<Journey, Long> {
+    List<Journey> findAllByUserId(Long id);
+    Journey findByUserIdAndJourneyType(Long id, JourneyType journeyType);
+}
