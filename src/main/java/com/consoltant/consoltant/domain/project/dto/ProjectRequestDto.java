5a760bb1daa1c7502ccffcb5495e6cdd7c62796f
박상욱 <dkxkqkrtkddn@naver.com>
2024-08-22 21:42:31 +0900

    feat: mapper expression 활용 contents Split & Join

--- src/main/java/com/consoltant/consoltant/domain/project/dto/ProjectRequestDto.java
+++ src/main/java/com/consoltant/consoltant/domain/project/dto/ProjectRequestDto.java
@@ -1,6 +1,7 @@
 package com.consoltant.consoltant.domain.project.dto;
 
 
+import com.consoltant.consoltant.domain.portfolio.entity.Portfolio;
 import com.consoltant.consoltant.domain.projectuser.dto.ProjectUserRequestDto;
 import java.time.LocalDate;
 import java.util.List;
@@ -16,8 +17,10 @@ public class ProjectRequestDto {
     private String language;
     private String projectUrl;
     private String description;
+    private List<String> contents;
     private LocalDate startDate;
     private LocalDate endDate;
     private List<ProjectUserRequestDto> projectUsers;
+
 
 }
