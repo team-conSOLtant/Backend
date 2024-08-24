d77dee07b86a6ff0f1642aef677b8c60d9a103ee
DDongYul <comeme0101@naver.com>
2024-08-24 23:03:30 +0900

    feat: 댓글 작성시 알림 전송 구현

--- src/main/java/com/consoltant/consoltant/domain/portfoliocomment/service/PortfolioCommentService.java
+++ src/main/java/com/consoltant/consoltant/domain/portfoliocomment/service/PortfolioCommentService.java
@@ -1,5 +1,7 @@
 package com.consoltant.consoltant.domain.portfoliocomment.service;
 
+import com.consoltant.consoltant.domain.notification.entity.Notification;
+import com.consoltant.consoltant.domain.notification.service.NotificationModuleService;
 import com.consoltant.consoltant.domain.portfolio.entity.Portfolio;
 import com.consoltant.consoltant.domain.portfolio.service.PortfolioModuleService;
 import com.consoltant.consoltant.domain.portfoliocomment.dto.PortfolioCommentRequestDto;
@@ -8,9 +10,11 @@ import com.consoltant.consoltant.domain.portfoliocomment.entity.PortfolioComment
 import com.consoltant.consoltant.domain.portfoliocomment.mapper.PortfolioCommentMapper;
 import com.consoltant.consoltant.domain.user.entity.User;
 import com.consoltant.consoltant.domain.user.repository.UserRepository;
+import com.consoltant.consoltant.util.constant.NotificationType;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -19,6 +23,7 @@ public class PortfolioCommentService {
     private final PortfolioCommentModuleService portfolioCommentModuleService;
     private final PortfolioModuleService portfolioModuleService;
     private final UserRepository userRepository;
+    private final NotificationModuleService notificationModuleService;
     private final PortfolioCommentMapper portfolioCommentMapper;
 
     // 단일 조회
@@ -34,12 +39,19 @@ public class PortfolioCommentService {
     }
 
     // 등록
+    @Transactional
     public PortfolioCommentResponseDto save(PortfolioCommentRequestDto portfolioCommentRequestDto) {
         PortfolioComment portfolioComment = portfolioCommentMapper.toPortfolioComment(portfolioCommentRequestDto);
         Portfolio portfolio = portfolioModuleService.findById(portfolioCommentRequestDto.getPortfolioId());
         User user = userRepository.findById(portfolioCommentRequestDto.getUserId()).orElseThrow();
         portfolioComment.setUser(user);
         portfolioComment.setPortfolio(portfolio);
+        //사용자 알림
+        notificationModuleService.save(Notification.builder()
+            .user(portfolio.getUser())
+            .notificationType(NotificationType.PORTFOLIO_COMMENT)
+            .content("포트폴리오에 댓글이 달렸습니다.")
+            .build());
         return portfolioCommentMapper.toPortfolioCommentResponseDto(portfolioCommentModuleService.save(portfolioComment));
     }
 
