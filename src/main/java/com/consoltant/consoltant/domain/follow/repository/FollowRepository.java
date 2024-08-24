1d3c9be867341298a0f21394dfcf29ff0cbc2788
DDongYul <comeme0101@naver.com>
2024-08-24 23:03:18 +0900

    feat: Follow 할 때 알림 전송, 이를 위한 알림타입 상수 추가, 이미 팔로우 한 대상 팔로우 안되도록 예외처리

--- src/main/java/com/consoltant/consoltant/domain/follow/repository/FollowRepository.java
+++ src/main/java/com/consoltant/consoltant/domain/follow/repository/FollowRepository.java
@@ -1,10 +1,14 @@
 package com.consoltant.consoltant.domain.follow.repository;
 
 import com.consoltant.consoltant.domain.follow.entity.Follow;
+import com.consoltant.consoltant.domain.portfolio.entity.Portfolio;
+import com.consoltant.consoltant.domain.user.entity.User;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FollowRepository extends JpaRepository<Follow, Long> {
     List<Follow> findAllByPortfolioId(Long id);
     List<Follow> findAllByUserId(Long id);
+
+    Boolean existsByUserAndPortfolio(User user, Portfolio portfolio);
 }
--- src/main/java/com/consoltant/consoltant/domain/follow/service/FollowService.java
+++ src/main/java/com/consoltant/consoltant/domain/follow/service/FollowService.java
@@ -4,13 +4,18 @@ import com.consoltant.consoltant.domain.follow.dto.FollowRequestDto;
 import com.consoltant.consoltant.domain.follow.dto.FollowResponseDto;
 import com.consoltant.consoltant.domain.follow.entity.Follow;
 import com.consoltant.consoltant.domain.follow.mapper.FollowMapper;
+import com.consoltant.consoltant.domain.notification.entity.Notification;
+import com.consoltant.consoltant.domain.notification.service.NotificationModuleService;
 import com.consoltant.consoltant.domain.portfolio.entity.Portfolio;
 import com.consoltant.consoltant.domain.portfolio.service.PortfolioModuleService;
 import com.consoltant.consoltant.domain.user.entity.User;
 import com.consoltant.consoltant.domain.user.repository.UserRepository;
+import com.consoltant.consoltant.global.exception.BadRequestException;
+import com.consoltant.consoltant.util.constant.NotificationType;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -19,6 +24,7 @@ public class FollowService {
     private final FollowModuleService followModuleService;
     private final FollowMapper followMapper;
     private final PortfolioModuleService portfolioModuleService;
+    private final NotificationModuleService notificationModuleService;
     private final UserRepository userRepository;
 
     // 유저 ID로 팔로잉 리스트 조회
@@ -36,12 +42,25 @@ public class FollowService {
     }
 
     // 등록
+    @Transactional
     public FollowResponseDto save(FollowRequestDto followRequestDto) {
         User user = userRepository.findById(followRequestDto.getUserId()).orElseThrow();
         Portfolio portfolio = portfolioModuleService.findById(followRequestDto.getPortfolioId());
+
+        // 이미 팔로우 중인지 확인
+        if (followModuleService.isAlreadyFollowing(user, portfolio)) {
+            throw new BadRequestException("이미 해당 포트폴리오를 팔로우하고 있습니다.");
+        }
+
         Follow follow = followMapper.toFollow(followRequestDto);
         follow.setPortfolio(portfolio);
         follow.setUser(user);
+        //사용자 알림
+        notificationModuleService.save(Notification.builder()
+            .user(portfolio.getUser())
+            .notificationType(NotificationType.PORTFOLIO_FOLLOW)
+            .content(user.getName() + " 님이 당신을 팔로우 했습니다.")
+            .build());
         return followMapper.toFollowResponseDto(followModuleService.save(follow));
     }
 
