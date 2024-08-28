db669bae2bcca20cc8d8844a79901cec7223a3ed
박상욱 <dkxkqkrtkddn@naver.com>
2024-08-28 21:11:46 +0900

    feat: 모범 로드맵 추천 및 찾기 API 개발 중

--- src/main/java/com/consoltant/consoltant/domain/bestroadmap/service/BestRoadmapModuleService.java
+++ src/main/java/com/consoltant/consoltant/domain/bestroadmap/service/BestRoadmapModuleService.java
@@ -1,2 +1,22 @@
-package com.consoltant.consoltant.domain.bestroadmap.service;public class BestRoadmapModuleService {
+package com.consoltant.consoltant.domain.bestroadmap.service;
+
+import com.consoltant.consoltant.domain.bestroadmap.dto.BestRoadmapResponseDto;
+import com.consoltant.consoltant.domain.bestroadmap.mapper.BestRoadmapMapper;
+import com.consoltant.consoltant.domain.bestroadmap.repository.BestRoadmapRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class BestRoadmapModuleService {
+    private final BestRoadmapRepository bestRoadmapRepository;
+    private final BestRoadmapMapper bestRoadmapMapper;
+
+    public List<BestRoadmapResponseDto> findAll(){
+        return bestRoadmapMapper.toBestRoadmapResponseDtoList(bestRoadmapRepository.findAll());
+    }
 }
--- src/main/java/com/consoltant/consoltant/domain/bestroadmap/service/BestRoadmapService.java
+++ src/main/java/com/consoltant/consoltant/domain/bestroadmap/service/BestRoadmapService.java
@@ -1,11 +1,174 @@
 package com.consoltant.consoltant.domain.bestroadmap.service;
 
+import com.consoltant.consoltant.domain.bestroadmap.entity.BestRoadmap;
+import com.consoltant.consoltant.domain.bestroadmap.repository.BestRoadmapRepository;
+import com.consoltant.consoltant.domain.journey.entity.Journey;
+import com.consoltant.consoltant.domain.journey.service.JourneyModuleService;
+import com.consoltant.consoltant.domain.portfolio.entity.Portfolio;
+import com.consoltant.consoltant.domain.portfolio.service.PortfolioModuleService;
+import com.consoltant.consoltant.domain.roadmap.entity.Roadmap;
+import com.consoltant.consoltant.domain.user.entity.User;
+import com.consoltant.consoltant.domain.user.repository.UserRepository;
+import com.consoltant.consoltant.global.exception.BadRequestException;
+import com.consoltant.consoltant.util.constant.FinanceKeyword;
+import com.consoltant.consoltant.util.constant.JourneyType;
+import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.util.Pair;
 import org.springframework.stereotype.Service;
+
+import java.util.*;
 
 @Service
 @Slf4j
 @RequiredArgsConstructor
 public class BestRoadmapService {
+    private final BestRoadmapRepository bestRoadmapRepository;
+    private final UserRepository userRepository;
+    private final PortfolioModuleService portfolioModuleService;
+    private final JourneyModuleService journeyModuleService;
+
+    //베스트 로드맵 생성
+    public void generateBestRoadmap(){
+
+        @AllArgsConstructor
+        class Standard{
+            FinanceKeyword financeKeyword;
+            Integer salary;
+            Long startAsset;
+        }
+
+        List<User> userList = userRepository.findAll();
+
+        PriorityQueue<Pair<Long, Pair<Standard,Integer>>> smallPQ = new PriorityQueue<>(Collections.reverseOrder());
+        PriorityQueue<Pair<Long, Pair<Standard,Integer>>> middlePQ = new PriorityQueue<>(Collections.reverseOrder());
+        PriorityQueue<Pair<Long, Pair<Standard,Integer>>> bigPQ = new PriorityQueue<>(Collections.reverseOrder());
+
+        Map<Standard, Long> roadmapStandard = new HashMap<>();
+
+        //TODO 소인 경우 대출 제외
+        for(User user : userList){
+
+            Integer salary = user.getSalary();
+
+            List<Journey> journeyList = journeyModuleService.findAllByUserId(user.getId())
+                    .stream()
+                    .sorted(Comparator.comparing(Journey::getAge))
+                    .toList();
+
+            Long startAsset = 0L;
+
+            JourneyType startJourneyType = journeyList.get(0).getJourneyType();
+            for(Journey journey : journeyList){
+                if(journey.getJourneyType() != startJourneyType){
+                    break;
+                }
+                startAsset += journeyList.get(0).getBalance();
+            }
+
+            Long smallAssetValue = 0L;
+            Integer smallAge = 0;
+
+            Long middleAssetValue = 0L;
+            Integer middleAge = 0;
+
+            Long bigAssetValue = 0L;
+            Integer bigAge = 0;
+
+            Integer startAge = journeyList.get(0).getAge();
+
+            //나이 별 자산
+            Map<Integer,Long> assetMap = new HashMap<>();
+
+            //소중대 자산 비교
+            for (Journey journey : journeyList) {
+                Integer age = journey.getAge();
+                assetMap.putIfAbsent(age, 0L);
+                assetMap.put(age, assetMap.get(age) + journey.getBalance());
+            }
+
+
+            for(Integer age: assetMap.keySet()){
+
+                //소중대 비교
+                if(assetMap.get(age) > smallAssetValue){
+                    smallAssetValue = assetMap.get(age);
+                    smallAge = age;
+                }
+
+                if(assetMap.get(age-4) != null){
+                    Long middleAsset = 0L;
+                    for(int i = age-4;i<=age;i++){
+                        middleAsset+=assetMap.get(i);
+                    }
+
+                    if(middleAsset > middleAssetValue){
+                        middleAssetValue = middleAsset;
+                        middleAge = age;
+                    }
+                }
+
+
+                if(assetMap.get(age-9) != null){
+                    Long bigAsset = 0L;
+                    for(int i = age-9;i<=age;i++){
+                        bigAsset+=assetMap.get(i);
+                    }
+
+                    if(bigAsset > bigAssetValue){
+                        bigAssetValue = bigAsset;
+                        bigAge = age;
+                    }
+                }
+            }
+
+            Standard small = new Standard(FinanceKeyword.SMALL_HAPPINESS,salary,startAsset);
+            Standard middle = new Standard(FinanceKeyword.MIDDLE_HAPPINESS,salary,startAsset);
+            Standard big = new Standard(FinanceKeyword.BIG_HAPPINESS,salary,startAsset);
+
+            smallPQ.add(Pair.of(smallAssetValue,Pair.of(small,smallAge)));
+            middlePQ.add(Pair.of(middleAssetValue,Pair.of(middle,middleAge)));
+            bigPQ.add(Pair.of(bigAssetValue,Pair.of(big,bigAge)));
+
+        }
+
+
+
+//
+//        Portfolio portfolio = portfolioModuleService.findByUserId(id).orElseThrow(()->new BadRequestException("존재하지 앟는 포트폴리오입니다."));
+//
+//        Long startAsset = journeyModuleService.findAllByUserId(id).get(0).getBalance();
+
+
+    }
+
+    // 베스트 로드맵 찾기
+    public Long findBestRoadmap(Long id) {
+
+        User user = userRepository.findById(id).orElseThrow(()->new BadRequestException("존재하지 앟는 사용자입니다."));
+        Portfolio portfolio = portfolioModuleService.findByUserId(id).orElseThrow(()->new BadRequestException("존재하지 앟는 포트폴리오입니다."));
+
+        List<Journey> journeyList = journeyModuleService.findAllByUserId(id);
+
+        Long startAsset = 0L;
+
+        JourneyType startJourneyType = journeyList.get(0).getJourneyType();
+        for(Journey journey : journeyList){
+            if(journey.getJourneyType() != startJourneyType){
+                break;
+            }
+            startAsset += journeyList.get(0).getBalance();
+        }
+
+        List<BestRoadmap> roadmapList = bestRoadmapRepository.findAllByFinanceKeyword(portfolio.getFinanceKeyword()).orElseThrow(()->new BadRequestException("베스트 로드맵이 존재하지 않습니다."));
+
+        Long finalStartAsset = startAsset;
+        List<BestRoadmap> bestRoadmapList  = roadmapList.stream()
+                .filter(s-> finalStartAsset - 200 <= s.getStartAsset() && s.getStartAsset() <= finalStartAsset + 200)
+                .filter(s->user.getSalary() - 50 <= s.getSalary() &&  s.getSalary() <= user.getSalary() + 20)
+                .toList();
+
+        return bestRoadmapList.get(0).getId();
+    }
 }
