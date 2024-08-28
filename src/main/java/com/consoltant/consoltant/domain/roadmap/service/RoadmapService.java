1b27d4f261b3daa3cff3d2ef7f7517d6ac66280e
박상욱 <dkxkqkrtkddn@naver.com>
2024-08-29 02:51:19 +0900

    feat: 예상 로드맵 기능 개발 중

--- src/main/java/com/consoltant/consoltant/domain/roadmap/service/RoadmapService.java
+++ src/main/java/com/consoltant/consoltant/domain/roadmap/service/RoadmapService.java
@@ -1,17 +1,14 @@
 package com.consoltant.consoltant.domain.roadmap.service;
 
-import ch.qos.logback.core.testUtil.RandomUtil;
 import com.consoltant.consoltant.domain.bestroadmap.dto.BestRoadmapResponseDto;
 import com.consoltant.consoltant.domain.bestroadmap.service.BestRoadmapModuleService;
-import com.consoltant.consoltant.domain.journey.dto.JourneyResponseDto;
 import com.consoltant.consoltant.domain.journey.entity.Journey;
 import com.consoltant.consoltant.domain.journey.repository.JourneyRepository;
 import com.consoltant.consoltant.domain.journey.service.JourneyModuleService;
 import com.consoltant.consoltant.domain.portfolio.entity.Portfolio;
 import com.consoltant.consoltant.domain.portfolio.service.PortfolioModuleService;
-import com.consoltant.consoltant.domain.product.dto.ProductListResponseDto;
-import com.consoltant.consoltant.domain.product.mapper.ProductMapper;
 import com.consoltant.consoltant.domain.product.service.ProductService;
+import com.consoltant.consoltant.domain.recommend.dto.RecommendResponseDto;
 import com.consoltant.consoltant.domain.recommend.service.RecommendModuleService;
 import com.consoltant.consoltant.domain.recommend.service.RecommendService;
 import com.consoltant.consoltant.domain.roadmap.dto.RoadmapGraphData;
@@ -22,8 +19,10 @@ import com.consoltant.consoltant.domain.roadmap.repository.RoadmapRepository;
 import com.consoltant.consoltant.domain.user.entity.User;
 import com.consoltant.consoltant.domain.user.repository.UserRepository;
 import com.consoltant.consoltant.global.exception.BadRequestException;
+import com.consoltant.consoltant.util.constant.JourneyType;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.util.Pair;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
@@ -74,7 +73,7 @@ public class RoadmapService {
     }
 
     //로드맵 그래프 생성
-    public RoadmapGraphResponseDto makeGraph(Long userId){
+    public RoadmapGraphResponseDto makeRoadmap(Long userId){
 
         User user = userRepository.findById(userId).orElseThrow(()->new BadRequestException("존재하지 않는 사용자입니다."));
 
@@ -86,24 +85,22 @@ public class RoadmapService {
 
         List<Journey> journeyList = journeyRepository.findAllByUserId(userId);
 
-        Map<Integer, List<Long>> assetList = new HashMap<>();
+        Map<Integer, Pair<List<Long>, JourneyType>> assetList = new HashMap<>();
 
         RoadmapGraphResponseDto roadmapGraphResponseDto = new RoadmapGraphResponseDto();
 
         for(Journey journey: journeyList){
             Integer age = journey.getAge();
             List<Long> assets = Arrays.asList(0L,0L,0L,0L);
-            assetList.putIfAbsent(age, assets);
+            assetList.putIfAbsent(age, Pair.of(assets,journey.getJourneyType()));
 
-
-            log.info("Integer -> {}",assetList.get(age).get(0));
             //총합 저장
-            assetList.get(age).set(0,assetList.get(age).get(0) + journey.getBalance());
+            assetList.get(age).getFirst().set(0,assetList.get(age).getFirst().get(0) + journey.getBalance());
 
            switch (journey.getProductType()){
-               case DEPOSIT -> {assetList.get(age).set(1,assetList.get(age).get(1) + journey.getBalance());}
-               case SAVING -> {assetList.get(age).set(2,assetList.get(age).get(2) + journey.getBalance());}
-               case LOAN -> {assetList.get(age).set(3,assetList.get(age).get(3) + journey.getBalance());}
+               case DEPOSIT -> {assetList.get(age).getFirst().set(1,assetList.get(age).getFirst().get(1) + journey.getBalance());}
+               case SAVING -> {assetList.get(age).getFirst().set(2,assetList.get(age).getFirst().get(2) + journey.getBalance());}
+               case LOAN -> {assetList.get(age).getFirst().set(3,assetList.get(age).getFirst().get(3) + journey.getBalance());}
            }
         }
 
@@ -114,7 +111,7 @@ public class RoadmapService {
 
         //여정
         for(Integer age : keySet){
-            roadmapGraphResponseDto.getData().add(new RoadmapGraphData(age,assetList.get(age).get(0),assetList.get(age).get(1),assetList.get(age).get(2),assetList.get(age).get(3)));
+            roadmapGraphResponseDto.getData().add(new RoadmapGraphData(age,assetList.get(age).getSecond(),assetList.get(age).getFirst().get(0),assetList.get(age).getFirst().get(1),assetList.get(age).getFirst().get(2),assetList.get(age).getFirst().get(3)));
         }
         
         //상품
@@ -154,5 +151,12 @@ public class RoadmapService {
         roadmapGraphResponseDto.setRecommend(recommendService.findAllByUserId(userId));
 
         return roadmapGraphResponseDto;
+    }
+
+    // 예상 로드맵 생성
+    public RoadmapGraphResponseDto makeExpectedRoadmap(Long userId, List<RecommendResponseDto> list){
+        //TODO list가 null이면 사용자가 가진 자산으로만 그래프 제공
+
+        return null;
     }
 }
